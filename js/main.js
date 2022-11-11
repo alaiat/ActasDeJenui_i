f1c9fb89a614fd6f99325bb545dab5c57989a418
alaia <alaiatorre2001@gmail.com>
2022-11-11 10:27:08 +0100

    Klaseko azkena

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
-hasieratu = function (e) {
-
-    console.log("a")
-    fetch("http://ikasten.io:3000/https://aenui.org/actas/indice_i.html").then(r=> r.text()).then(r=> {
-       //Get index.html from aenui.org
-       document.getElementsByTagName("html")[0].innerHTML=r
+async function getInitialHTML(){
+    let a= await fetch("http://ikasten.io:3000/https://aenui.org/actas/indice_i.html").then(r=> r.text()).then(r=> {
+        //Get index.html from aenui.org
+        document.getElementsByTagName("html")[0].innerHTML=r
         //Get the image from the index.html working
-        document.querySelector("img[src='img/logoAenui.png']").src="https://aenui.org/actas/img/logoAenui.png"
         //Add boostrap to the page
         var link = document.createElement('link');
         link.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
@@ -14,19 +11,17 @@ hasieratu = function (e) {
         link.crossOrigin = "anonymous"
         document.head.appendChild(link);
 
+    })
+    console.log(a)
+    return a;
+}
+
+hasieratu = function (e) {
+    let html=getInitialHTML();
+    console.log(html);
+    document.querySelector("img[src='img/logoAenui.png']").src="https://aenui.org/actas/img/logoAenui.png"
 
 
-
-
-
-
-
-
-
-
-
-
-    });
 
 
 
