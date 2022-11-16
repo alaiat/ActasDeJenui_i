1fd50372f44fe49a462e086dd57e0e05efe79b2f
aitorcas23 <aitorcas23@gmail.com>
2022-11-16 18:09:11 +0100

    maldito file no escribe

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
+const mainURL = 'aenui.org/actas'
+
 async function getInitialHTML(){
     let a= await fetch("http://ikasten.io:3000/https://aenui.org/actas/indice_i.html").then(r=> r.text()).then(r=> {
         //Get index.html from aenui.org
         document.getElementsByTagName("html")[0].innerHTML=r
         //Get the image from the index.html working
         //Add boostrap to the page
-        var link = document.createElement('link');
+        let link = document.createElement('link');
         link.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
         link.rel = "stylesheet"
         link.integrity = "sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
@@ -16,22 +18,25 @@ async function getInitialHTML(){
     return a;
 }
 
-hasieratu = function (e) {
-    let html=getInitialHTML();
+async function getJs(name){
+    name = name.split('/')
+    name[2] = mainURL
+    name = name.join('/')
+    fetch(`http://ikasten.io:3000/${name}`).then(r=>r.text())
+    .then(r=> {
+        console.log(r)
+        console.log(name.split('/')[5])
+        let f = new File(name.split('/')[5].split('.')[0],'a')
+        
+        f.writeFile(r)
+    })
+}
+
+let hasieratu = async function (e) {
+    let html= await getInitialHTML();
     console.log(html);
     document.querySelector("img[src='img/logoAenui.png']").src="https://aenui.org/actas/img/logoAenui.png"
-
-
-
-
-
-
-
-
-
-
-
-
+    document.querySelectorAll("script").forEach(elem => getJs(elem.src))
 
 }
 
