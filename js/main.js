c101335c38286f6c9e728f9704b8a8888d643727
aitorcas23 <aitorcas23@gmail.com>
2022-12-22 19:54:30 +0100

    Por fin hace cosas

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ const mainURL = 'aenui.org/actas'
 async function getInitialHTML(){
     let a= await fetch("http://ikasten.io:3000/https://aenui.org/actas/indice_i.html").then(r=> r.text()).then(r=> {
         //Get index.html from aenui.org
+
+        //r = r.replace('<img src="', '<img src="https://aenui.org/actas/')
+
         document.getElementsByTagName("html")[0].innerHTML=r
         //Get the image from the index.html working
         //Add boostrap to the page
@@ -14,32 +17,56 @@ async function getInitialHTML(){
         link.integrity = "sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
         link.crossOrigin = "anonymous"
         document.head.appendChild(link);
-
     })
-    console.log(a)
     return a;
 }
 
-async function getAllJs(){
-    document.querySelectorAll("script").forEach(async elem => {
-        let name = elem.src
-        name = name.split("/")
-        name[2] = mainURL
-        name = name.join('/')
-        await fetch(`http://ikasten.io:3000/${name}`).then(r=>r.text())
-        .then(r=> {
-            let script = document.createElement('script')
-            script.innerHTML=r
-            document.head.appendChild(script)
-        })
+async function getAllJs(scripts){
+    let elem = scripts.shift()
+    let name = elem.src
+    name = name.split("/")
+    name[2] = mainURL
+    name = name.join('/')
+    await fetch(`http://ikasten.io:3000/${name}`).then(r=>r.text())
+    .then(r=> {
+        let script = document.createElement('script')
+        if(name.split("/")[name.split("/").length-1] == 'ui.js'){
+            r = r.replace('var urlFicha = "fichas/" + articulos[indiz][0] + "_" + articulos[indiz][1] + "_" + num + ".html";',
+             `var urlFicha = "https://aenui.org/actas/fichas/" + articulos[indiz][0] + "_" + articulos[indiz][1] + "_" + num + ".html";`)
+        }else if(name.split("/")[name.split("/").length-1] == 'urlFiles.js'){
+            r = r.replaceAll('return "', 'return "https://aenui.org/actas/')
+        }
+        script.innerHTML=r
+        document.head.appendChild(script)
+        if(scripts.length > 0){
+            getAllJs(scripts)
+        }else{
+            inicializar()
+        }
+        
     })
 }
 
 let hasieratu = async function (e) {
     let html= await getInitialHTML();
-    console.log(html);
     document.querySelector("img[src='img/logoAenui.png']").src="https://aenui.org/actas/img/logoAenui.png"
-    getAllJs().then(r => inicializar())
+    document.getElementById("botonesEdiciones").style = "display: none;"
+    let botones = document.getElementById("botonesEdiciones").getElementsByClassName("botonEdicion")
+    setTimeout(()=>{
+        console.log(Array.from(botones))
+        Array.from(botones).forEach((elem)=>{
+            let url = elem.children[0].src.split('/')
+            console.log(url)
+            url[2] = "aenui.org"
+            url.splice(3, 0, "actas")
+            elem.children[0].src = url.join('/')
+            console.log(elem.children[0].src)
+        })
+    }, 1000)
+    
+    let scripts = document.querySelectorAll("script")
+    getAllJs(Array.from(scripts))
+    //.then(r => inicializar())
 }
 
 
