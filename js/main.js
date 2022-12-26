561efd686c77ff5e269615d1bd24ab46dc49b2d0
aitorcas23 <aitorcas23@gmail.com>
2022-12-26 17:26:19 +0100

    La ficha aparece debajo del titulo

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
- 
+
 
 const mainURL = 'aenui.org/actas'
 
-async function getInitialHTML(){
-    let a= await fetch("http://ikasten.io:3000/https://aenui.org/actas/indice_i.html").then(r=> r.text()).then(r=> {
+async function getInitialHTML() {
+    let a = await fetch("http://ikasten.io:3000/https://aenui.org/actas/indice_i.html").then(r => r.text()).then(r => {
         //Get index.html from aenui.org
 
         //r = r.replace('<img src="', '<img src="https://aenui.org/actas/')
 
-        document.getElementsByTagName("html")[0].innerHTML=r
+        document.getElementsByTagName("html")[0].innerHTML = r
         //Get the image from the index.html working
         //Add boostrap to the page
         let link = document.createElement('link');
         link.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
         link.rel = "stylesheet"
         let link2 = document.createElement("link")
-        link2.href = "https://getbootstrap.com/docs/5.2/assets/css/docs.css" 
+        link2.href = "https://getbootstrap.com/docs/5.2/assets/css/docs.css"
         link2.rel = "stylesheet"
         document.head.appendChild(link);
         document.head.appendChild(link2);
@@ -23,65 +23,70 @@ async function getInitialHTML(){
     return a;
 }
 
-async function getAllJs(scripts){
+async function getAllJs(scripts) {
     let elem = scripts.shift()
     let name = elem.src
     name = name.split("/")
     name[2] = mainURL
     name = name.join('/')
-    await fetch(`http://ikasten.io:3000/${name}`).then(r=>r.text())
-    .then(r=> {
-        let script = document.createElement('script')
-        if(name.split("/")[name.split("/").length-1] == 'ui.js'){
-            r = r.replace('var urlFicha = "fichas/" + articulos[indiz][0] + "_" + articulos[indiz][1] + "_" + num + ".html";',
-             `var urlFicha = "https://aenui.org/actas/fichas/" + articulos[indiz][0] + "_" + articulos[indiz][1] + "_" + num + ".html";`)
-        }else if(name.split("/")[name.split("/").length-1] == 'urlFiles.js'){
-            r = r.replaceAll('return "', 'return "https://aenui.org/actas/')
-        }else if(name.split("/")[name.split("/").length-1] == 'search.js'){
-            r = r.replace('temasBusqueda[i] = convertirPaBusqueda(articulos[i][2] + " " + articulos[i][3]);',
-            'temasBusqueda[i] = {titulo: convertirPaBusqueda(articulos[i][2]), autor: convertirPaBusqueda(articulos[i][3]), palabras: convertirPaBusqueda(articulos[i][5] + " " + articulos[i][6])}')
-            console.log(r)
-            
-            r = r.replace('\x69\x66\x20\x28\x69\x74\x65\x6D\x2E\x69\x6E\x64\x65\x78\x4F\x66\x28\x73\x74\x72\x29\x20\x21\x3D\x20\x2D\x31\x29\x0A\x09\x09\x09\x09\x6C\x6C\x69\x73\x74\x61\x2E\x70\x75\x73\x68\x28\x69\x29\x3B',
-            'titulo = document.getElementById("checkbox3").checked; autor=document.getElementById("checkbox1").checked; palabras = document.getElementById("checkbox2").checked; if(!titulo && !autor && !palabras){titulo = true; autor = true; palabras = true;} addToList = false; if(titulo && item.titulo.indexOf(str) != -1){addToList = true;} if(autor && item.autor.indexOf(str) != -1){addToList = true;} if(palabras && item.palabras.indexOf(str) != -1){addToList = true;} if(addToList){llista.push(i);}')
-        }
-        script.innerHTML=r
-        document.head.appendChild(script)
-        if(scripts.length > 0){
-            getAllJs(scripts)
-        }else{
-            inicializar()
-        }
-        
-    })
+    await fetch(`http://ikasten.io:3000/${name}`).then(r => r.text())
+        .then(r => {
+            let script = document.createElement('script')
+            if (name.split("/")[name.split("/").length - 1] == 'ui.js') {
+                r = r.replace('var urlFicha = "fichas/" + articulos[indiz][0] + "_" + articulos[indiz][1] + "_" + num + ".html";',
+                    `var urlFicha = "https://aenui.org/actas/fichas/" + articulos[indiz][0] + "_" + articulos[indiz][1] + "_" + num + ".html";`)
+
+                r = r.replace("function amosarFicha(indiz) {", "function amosarFicha(indiz) { titleOnclick(indiz);")
+
+                //r = r.replace('document.getElementById("ficha").innerHTML' ,'document.getElementById("ficha").innerHTML')
+            } else if (name.split("/")[name.split("/").length - 1] == 'urlFiles.js') {
+                r = r.replaceAll('return "', 'return "https://aenui.org/actas/')
+            } else if (name.split("/")[name.split("/").length - 1] == 'search.js') {
+                r = r.replace('temasBusqueda[i] = convertirPaBusqueda(articulos[i][2] + " " + articulos[i][3]);',
+                    'temasBusqueda[i] = {titulo: convertirPaBusqueda(articulos[i][2]), autor: convertirPaBusqueda(articulos[i][3]), palabras: convertirPaBusqueda(articulos[i][5] + " " + articulos[i][6])}')
+                console.log(r)
+
+                r = r.replace('\x69\x66\x20\x28\x69\x74\x65\x6D\x2E\x69\x6E\x64\x65\x78\x4F\x66\x28\x73\x74\x72\x29\x20\x21\x3D\x20\x2D\x31\x29\x0A\x09\x09\x09\x09\x6C\x6C\x69\x73\x74\x61\x2E\x70\x75\x73\x68\x28\x69\x29\x3B',
+                    'titulo = document.getElementById("checkbox3").checked; autor=document.getElementById("checkbox1").checked; palabras = document.getElementById("checkbox2").checked; if(!titulo && !autor && !palabras){titulo = true; autor = true; palabras = true;} addToList = false; if(titulo && item.titulo.indexOf(str) != -1){addToList = true;} if(autor && item.autor.indexOf(str) != -1){addToList = true;} if(palabras && item.palabras.indexOf(str) != -1){addToList = true;} if(addToList){llista.push(i);}')
+            }
+            script.innerHTML = r
+            document.head.appendChild(script)
+            if (scripts.length > 0) {
+                getAllJs(scripts)
+            } else {
+                inicializar()
+            }
+
+        })
 }
 
 let hasieratu = async function (e) {
-    let html= await getInitialHTML();
-    document.querySelector("img[src='img/logoAenui.png']").src="https://aenui.org/actas/img/logoAenui.png"
+    let html = await getInitialHTML();
+    document.querySelector("img[src='img/logoAenui.png']").src = "https://aenui.org/actas/img/logoAenui.png"
     document.getElementById("botonesEdiciones").style = "display: none;"
     let botones = document.getElementById("botonesEdiciones").getElementsByClassName("botonEdicion")
-    setTimeout(()=>{
-        Array.from(botones).forEach((elem)=>{
+    setTimeout(() => {
+        Array.from(botones).forEach((elem) => {
             let url = elem.children[0].src.split('/')
             url[2] = "aenui.org"
             url.splice(3, 0, "actas")
             elem.children[0].src = url.join('/')
         })
     }, 1000)
-    
+
     let scripts = document.querySelectorAll("script")
     getAllJs(Array.from(scripts))
     //.then(r => inicializar())
     centerTitle()
     addNav()
     addFiltros()
+    changeFichaName()
 
     //Este tiene que ir último
     addScripts()
 }
 
-function centerTitle(){
+function centerTitle() {
     let buscador = document.getElementById("buscaor")
 
     let buscadorRow = document.createElement("div")
@@ -97,7 +102,7 @@ function centerTitle(){
     buscadorCol.setAttribute("id", "buscadorCol")
     let buscadorTd = buscador.children.item(0).children.item(0).children.item(0).children.item(1)
     Array.from(buscadorTd.children).forEach(elem => {
-        if(elem.tagName != "input"){
+        if (elem.tagName != "input") {
             elem.setAttribute("class", "text-center")
         }
         buscadorCol.appendChild(elem)
@@ -122,9 +127,9 @@ function centerTitle(){
     document.body.appendChild(resultaos)
 }
 
-function addNav(){
+function addNav() {
     let style = document.createElement("style")
-    style.innerHTML= "body {padding-top: 60px;}.offcanvas-size-sm {--bs-offcanvas-width: 150px !important;} .login-button {margin-bottom: 6px; margin-right: 0px;} @media only screen and (min-width: 576px) {.login-button {margin-bottom: 0px;margin-right: 6px;}}.bolded { font-weight: bold; }"
+    style.innerHTML = "body {padding-top: 60px;}.offcanvas-size-sm {--bs-offcanvas-width: 150px !important;} .login-button {margin-bottom: 6px; margin-right: 0px;} @media only screen and (min-width: 576px) {.login-button {margin-bottom: 0px;margin-right: 6px;}}.bolded { font-weight: bold; }"
     document.head.appendChild(style)
     let navBar = document.createElement("header")
     navBar.setAttribute("class", "navbar navbar-light fixed-top navbar-default navbar-expand-sm")
@@ -174,7 +179,7 @@ function addNav(){
     menuBody.setAttribute("class", "offcanvas-body py-0 col justify-content-sm-end")
     menuBody.appendChild(bodyCol)
     let menuHead = document.createElement("div")
-    menuHead.setAttribute("class","offcanvas-header bolded")
+    menuHead.setAttribute("class", "offcanvas-header bolded")
     let offLabel = document.createElement("h2")
     offLabel.setAttribute("id", "offLabel")
     offLabel.setAttribute("class", "offcanvas-title")
@@ -208,7 +213,7 @@ function addNav(){
     document.getElementsByClassName("botonEdiciones").item(0).remove()
 }
 
-function addScripts(){
+function addScripts() {
 
     let bootstrapScript = document.createElement("script")
     bootstrapScript.setAttribute("src", "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js")
@@ -216,7 +221,7 @@ function addScripts(){
 }
 
 //Activa otra vez el amosar resultados para actualizar busqueda cuando cambias los filtros
-function reSerch(){
+function reSerch() {
     amosarResultaosPorTestu(document.getElementById("caxaBuscar").value, false)
 }
 
@@ -228,7 +233,7 @@ function addFiltros() {
     checkbox1.setAttribute("id", "checkbox1")
     checkbox1.setAttribute("style", "margin-right: 3px")
     checkbox1.onclick = reSerch
-  
+
     let label1 = document.createElement("label")
     label1.setAttribute("class", "form-check-label")
     label1.setAttribute("for", "checkbox1")
@@ -239,14 +244,14 @@ function addFiltros() {
     col1.setAttribute("class", "col")
     col1.appendChild(checkbox1)
     col1.appendChild(label1)
-  
+
     let checkbox2 = document.createElement("input")
     checkbox2.setAttribute("type", "checkbox")
     checkbox2.setAttribute("class", "form-check-input")
     checkbox2.setAttribute("id", "checkbox2")
     checkbox2.setAttribute("style", "margin-right: 3px")
     checkbox2.onclick = reSerch
-  
+
     let label2 = document.createElement("label")
     label2.setAttribute("class", "form-check-label")
     label2.setAttribute("for", "checkbox2")
@@ -257,14 +262,14 @@ function addFiltros() {
     col2.setAttribute("class", "col")
     col2.appendChild(checkbox2)
     col2.appendChild(label2)
-  
+
     let checkbox3 = document.createElement("input")
     checkbox3.setAttribute("type", "checkbox")
     checkbox3.setAttribute("class", "form-check-input")
     checkbox3.setAttribute("id", "checkbox3")
     checkbox3.setAttribute("style", "margin-right: 3px")
     checkbox3.onclick = reSerch
-  
+
     let label3 = document.createElement("label")
     label3.setAttribute("class", "form-check-label")
     label3.setAttribute("for", "checkbox3")
@@ -275,7 +280,7 @@ function addFiltros() {
     col3.setAttribute("class", "col")
     col3.appendChild(checkbox3)
     col3.appendChild(label3)
-  
+
     let buscadorCol = document.getElementById("buscadorCol")
     let row = document.createElement("div")
     row.setAttribute("class", "row row-cols-sm-auto row-cols-1")
@@ -283,6 +288,24 @@ function addFiltros() {
     row.appendChild(col2)
     row.appendChild(col3)
     buscadorCol.appendChild(row)
-  }
+}
 
-window.onload=hasieratu;
+function changeFichaName(){
+    document.getElementById("ficha").setAttribute("id", "fich")
+}
+
+function titleOnclick(indiz){
+    let titulu = document.getElementsByClassName("tituluPonencia")
+    Array.from(titulu).forEach(elem =>{
+        numberSplit = elem.getAttribute("onclick").split("(")[1].split('')
+        numberSplit.pop()
+        number = numberSplit.join('')
+        if(number == indiz){
+            let ficha = document.getElementById("fich").cloneNode(true)
+            ficha.setAttribute("id", "ficha")
+            elem.appendChild(ficha)
+        }
+    })
+}
+
+window.onload = hasieratu;
